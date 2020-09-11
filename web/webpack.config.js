b5a1eee561705e1b51ef66d7a983bf98cfc06850
Paul Ye <oatto.paul@gmail.com>
2020-09-11 14:29:31 +0200

    Enable source maps during development

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,27 +1,35 @@
 const path = require('path');
 
-module.exports = {
-  entry: {
-    polyfills: './js/polyfills.js',
-    index: './js/codeworld.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = () => {
+  const mode = process.env.NODE_ENV;
+  const isProduction = mode === 'production';
+
+  return {
+    entry: {
+      polyfills: './js/polyfills.js',
+      index: './js/codeworld.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    mode,
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
